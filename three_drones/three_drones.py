ef0869a8a2ef7e77ffeeb018442e93987e9c4340
CL-Chong <ching.lok.chong.secondary@gmail.com>
2023-10-31 22:12:12 +0000

    used list to store lh and dlh. fixed typos.

--- three_drones/three_drones.py
+++ three_drones/three_drones.py
@@ -60,19 +60,22 @@ def stlog_symbolic(sys, order):
     v = casadi.MX.sym("v", sys.NU)
     T = casadi.MX.sym("T")
     stlog = casadi.MX.zeros(sys.nx, sys.nx)
-    lh_store = casadi.MX.zeros(sys.ny, order + 1)
-    lh_store[:, 0] = sys.observation(x)
+    lh_store = []
+    lh_store.append(sys.observation(x))
+    dlh_store = []
+    dlh_store.append(casadi.jacobian(lh_store[0], x))
 
     for l in range(0, order):
-        lh_store[:, l + 1] = casadi.jtimes(lh_store[:, l], x, sys.dynamics(x, v, u))
+        lh_store.append(casadi.jtimes(lh_store[l], x, sys.dynamics(x, v, u)))
+        dlh_store.append(casadi.jacobian(lh_store[l + 1], x))
 
     for l in range(0, order):
         for k in range(0, l + 1):
             stlog += (
-                (T ** (l + 1)) / ((l + 1) * math.factorial(k) * math.factorial(l))
+                (T ** (l + 1)) / ((l + 1) * math.factorial(k) * math.factorial(l - k))
             ) * casadi.mtimes(
-                casadi.jacobian(lh_store[:, k], x).T,
-                casadi.jacobian(lh_store[:, l - k], x),
+                dlh_store[k].T,
+                dlh_store[l - k],
             )
 
     stlog_fun = casadi.Function("stlog_fun", [x, v, u, T], [stlog])
