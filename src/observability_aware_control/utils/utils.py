3b9c878b906c35dfb8c1ddfc73518ec71f04df43
Hs293Go <hellston20a@gmail.com>
2023-11-25 00:54:23 -0500

    Utils: More efficient angle wrapping function

--- src/observability_aware_control/utils/utils.py
+++ src/observability_aware_control/utils/utils.py
@@ -7,6 +7,21 @@ from numpy.typing import ArrayLike
 Archive = Dict[str, ArrayLike]
 
 
+def wrap_to_2pi(angle):
+    angle = np.atleast_1d(angle)
+    positive_input = angle > 0
+    angle = np.mod(angle, 2 * np.pi)
+    angle[(angle == 0) & positive_input] = 2 * np.pi
+    return angle
+
+
+def wrap_to_pi(angle):
+    angle = np.atleast_1d(angle)
+    q = (angle < -np.pi) | (np.pi < angle)
+    angle[q] = wrap_to_2pi(angle[q] + np.pi) - np.pi
+    return angle
+
+
 def prefix_key(d: Dict[str, Any], prefix: str):
     return {f"{prefix}.{k}": v for k, v in d.items()}
 
