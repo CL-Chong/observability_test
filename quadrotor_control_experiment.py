acf490aa238dd5b14f1d0d67c9e26d0af1bbd543
Hs293Go <hellston20a@gmail.com>
2024-01-09 23:13:26 +0800

    Stable quadrotor control experiment

--- quadrotor_control_experiment.py
+++ quadrotor_control_experiment.py
@@ -1,4 +1,5 @@
 import math
+import sys
 
 import jax
 import jax.numpy as jnp
@@ -32,36 +33,18 @@ cc.initialize_cache("./.cache")
 jax.config.update("jax_enable_x64", True)
 
 
+u_eqm = np.r_[9.81, 0.0, 0.0, 0.0]
+q_eqm = np.r_[np.zeros(3), 1.0]
+v_eqm = np.zeros(3)
+
+
 def main():
     with open("./config/quadrotor_control_experiment.toml", "rb") as fp:
         cfg = tomllib.load(fp)
 
     mdl = multi_quadrotor.MultiQuadrotor(
-        cfg["model"]["n_robots"], cfg["model"]["robot_mass"]
+        cfg["model"]["n_robots"], cfg["model"]["robot_mass"], has_odom=True
     )
-
-    # -----------------------Generate initial trajectory------------------------
-
-    trajectory_data = np.load(cfg["initial_path"]["leader_trajectory"])
-    u_leader = trajectory_data["inputs"]
-    t_sample = trajectory_data["time"]
-    dt = np.diff(t_sample)[0]
-
-    # -----------------Setup initial conditions and data saving-----------------
-    sim_steps = cfg["sim"]["steps"]
-    time = t_sample[0:sim_steps]
-    x = np.zeros((sim_steps, mdl.nx))
-    u = np.zeros((sim_steps, mdl.nu))
-    x[0, :] = np.concatenate(
-        [
-            np.r_[it, np.zeros(3), 1.0, np.zeros(3)]
-            for it in cfg["initial_path"]["initial_positions"]
-        ]
-    )
-    u[0, :] = np.tile(np.array([9.81, 0.0, 0.0, 0.0]), 3)
-    status = []
-    nit = []
-    fun_hists = []
 
     # ---------------------------Setup the Optimizer----------------------------
     stlog = STLOG(mdl, cfg["stlog"]["order"])
@@ -74,7 +57,7 @@ def main():
         id_leader=0,
         lb=u_lb,
         ub=u_ub,
-        obs_comps=cfg["opc"]["observed_components"],
+        obs_comps=cfg["opc"].get("observed_components", ()),
         method=cfg["optim"]["method"],
         optim_options=cfg["optim"]["options"],
         min_v2v_dist=cfg["opc"]["min_inter_vehicle_distance"],
@@ -84,48 +67,98 @@ def main():
     min_problem = CooperativeOPCProblem(stlog, opts)
     anim = utils.anim_utils.Animated3DTrajectory(mdl.n_robots)
 
-    # ----------------------------Run the Simulation----------------------------
-    with plt.ion():
-        for i in tqdm.tqdm(range(1, sim_steps)):
-            soln = min_problem.minimize(
-                x[i - 1, :],
-                jnp.broadcast_to(u[i - 1, :], (window, len(u[i - 1, :]))),
-                dt,
-            )
-            soln_u = np.concatenate([u_leader[:, i - 1], soln.x[0, mdl.robot_nu :]])
+    # -----------------------Generate initial trajectory------------------------
+    trajectory_data = np.load(cfg["session"]["leader_trajectory"])
+    u_leader = trajectory_data["inputs"]
+    t_sample = trajectory_data["time"]
+    dt = np.diff(t_sample)[0]
 
-            soln_stats["status"].append(soln.status)
-            soln_stats["nit"].append(soln.get("nit", np.nan))
-            soln_stats["execution_time"].append(soln.get("execution_time", np.nan))
-            soln_stats["constr_violation"].append(soln.get("constr_violation", np.nan))
-            soln_stats["optimality"].append(soln.get("optimality", np.nan))
+    # -----------------Setup initial conditions and data saving-----------------
+    sim_steps = cfg["sim"]["steps"]
+    time = t_sample[0:sim_steps]
+    x = np.zeros((sim_steps, mdl.nx))
+    u = np.zeros((sim_steps, mdl.nu))
 
-            fun_hist = np.full(cfg["optim"]["options"]["maxiter"], soln.fun)
-            fun_hist[0 : len(soln.fun_hist)] = np.asarray(soln.fun_hist)
-            fun_hists.append(np.array(fun_hist))
-            u[i, :] = soln_u
-            x[i, :] = forward_dynamics(mdl.dynamics, x[i - 1, :], soln_u, dt, "RK4")
+    n_ic = len(cfg["session"]["initial_positions"])
+    if n_ic != mdl.n_robots:
+        print(f"Incorrect number of initial positions {n_ic} for {mdl.n_robots} robots")
+        sys.exit(1)
 
-            plt_data = np.reshape(x[0:i, :], (i, mdl.n_robots, mdl.robot_nx))
-            anim.annotation = (
-                f"f(x): {soln.fun:.4}\nOptimality: {soln.optimality:.4}\ngnorm:"
-                f" {np.linalg.norm(soln.grad):.4}\nviolation: {soln.constr_violation:.4}"
-            )
-            anim.t = time[0:i]
-            for idx in range(mdl.n_robots):
-                anim.x[idx] = plt_data[:, idx, 0]
-                anim.y[idx] = plt_data[:, idx, 1]
-                anim.z[idx] = plt_data[:, idx, 2]
-            plt.pause(1e-3)
-
-    soln_stats = {k: np.asarray(v) for k, v in soln_stats.items()}
-    np.savez(
-        "data/optimization_results.npz",
-        states=x,
-        inputs=u,
-        time=time,
-        **soln_stats,
+    x[0, :] = np.concatenate(
+        [np.r_[it, q_eqm, v_eqm] for it in cfg["session"]["initial_positions"]]
     )
+
+    input_steps, nu = u_leader.shape
+    if nu != mdl.robot_nu:
+        print(f"Incorrect quad trajectory input size {nu} vs {mdl.robot_nu}")
+        sys.exit(1)
+    if sim_steps + window > input_steps:
+        u_leader_tmp = np.array(u_leader)
+        u_leader = np.zeros((sim_steps + window, mdl.robot_nu))
+        u_leader[:input_steps, :] = u_leader_tmp
+        u_leader[input_steps:, :] = u_leader[-1, :]
+
+    u0 = np.r_[u_leader[0, :], np.tile(u_eqm, 2)]
+    u[0, :] = u0
+
+    soln_stats = {
+        "status": [],
+        "nit": [],
+        "fun_hist": [],
+        "execution_time": [],
+        "constr_violation": [],
+        "optimality": [],
+    }
+
+    # ----------------------------Run the Simulation----------------------------
+    success = False
+    try:
+        with plt.ion():
+            for i in tqdm.trange(1, sim_steps):
+                u_leader_0 = u_leader[i - 1 : i - 1 + window, :]
+
+                # u0 = jnp.hstack([u_leader_0, np.tile(u_eqm, (window, 2))])
+                u0 = jnp.tile(u_leader_0, (1, 3))
+                soln = min_problem.minimize(x[i - 1, :], u0, dt)
+                soln_u = np.concatenate([u_leader[i - 1, :], soln.x[0, mdl.robot_nu :]])
+                u[i, :] = soln_u
+                x[i, :] = forward_dynamics(
+                    mdl.dynamics, x[i - 1, :], soln_u, dt, "euler"
+                )
+
+                fun = soln.fun
+                status = soln.get("status", -1)
+                nit = soln.get("nit", np.nan)
+                execution_time = soln.get("execution_time", np.nan)
+                constr_violation = float(soln.get("constr_violation", np.nan))
+                optimality = soln.get("optimality", np.nan)
+
+                soln_stats["status"].append(status)
+                soln_stats["nit"].append(nit)
+                soln_stats["execution_time"].append(execution_time)
+                soln_stats["constr_violation"].append(constr_violation)
+                soln_stats["optimality"].append(optimality)
+
+                fun_hist = np.full(cfg["optim"]["options"]["maxiter"], fun)
+                fun_hist[0 : len(soln.fun_hist)] = np.asarray(soln.fun_hist)
+                soln_stats["fun_hist"].append(fun_hist)
+
+                anim.annotation = f"nit: {nit} f(x): {fun:.4}\n $\\Delta$ f(x): {(fun - fun_hist[0]):4g}\nOptimality: {optimality:.4}\nviolation: {constr_violation:.4}"
+                plt_data = np.reshape(x[0:i, :], (i, mdl.n_robots, mdl.robot_nx))
+
+                anim.t = time[0:i]
+                for idx in range(mdl.n_robots):
+                    anim.x[idx] = plt_data[:, idx, 0]
+                    anim.y[idx] = plt_data[:, idx, 1]
+                    anim.z[idx] = plt_data[:, idx, 2]
+                plt.pause(1e-3)
+            success = True
+    finally:  # Save the data at all costs
+        soln_stats = {k: np.asarray(v) for k, v in soln_stats.items()}
+        save_name = str(cfg["session"].get("save_name", "optimization_results.npz"))
+        if not success:
+            save_name = save_name.replace(".npz", ".failed.npz")
+        np.savez(save_name, states=x, inputs=u, time=time, **soln_stats)
 
     figs = {}
     figs[0], ax = plt.subplots(subplot_kw={"projection": "3d"})
